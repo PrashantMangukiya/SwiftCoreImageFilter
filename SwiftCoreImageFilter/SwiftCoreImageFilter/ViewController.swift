583642fb8325084e7f4644c8ec553abf43553a08
PrashantKumar Mangukiya <pbMangukiya@gmail.com>
2015-11-16 10:02:57 +0530

    Initial commit: Source code folder added (code not finalised yet).
    Source code folder added named SwiftCoreImageFilter. This commit
    includes user element placement on the screen as per application flow.
    Programming logic not added yet.

---
+++ SwiftCoreImageFilter/SwiftCoreImageFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCoreImageFilter
+//
+//  Created by Prashant on 16/11/15.
+//  Copyright © 2015 PrashantKumar Mangukiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
